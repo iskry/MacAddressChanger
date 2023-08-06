253c521ab43e514fe88bc27c4ec258b5cf1af226
iskry <refocuslucifer@protonmail.com>
2023-08-06 19:25:59 -0400

    configured commands to include variables

--- mac.changer.py
+++ mac.changer.py
@@ -2,13 +2,13 @@
 
 import subprocess
 
-interface = "wlan0"
+interface = "eth0"
 newMac = "00:11:22:33:44:77"
 
 
 print("[+} Changing MAC addres for " + interface + " to " + newMac)
 
-#subprocess.call("ifconfig wlan0 down", shell=True)
-#subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-#subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
