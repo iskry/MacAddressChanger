f5b36a6548893e91b8d4a9d38eb588f354cd6532
iskry <refocuslucifer@protonmail.com>
2023-08-06 19:19:42 -0400

    added variables for network interface and new MAC address. will begin to make code more dynamic

--- mac.changer.py
+++ mac.changer.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+interface = "wlan0"
+newMac = "00:11:22:33:44:77"
+
+
+print("[+} Changing MAC addres for " + interface + " to " + newMac)
+
+#subprocess.call("ifconfig wlan0 down", shell=True)
+#subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
+#subprocess.call("ifconfig wlan0 up", shell=True)
 
