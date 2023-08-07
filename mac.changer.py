50f7df5d91c3b17074bfb8cbffa176169bf83c33
iskry <refocuslucifer@protonmail.com>
2023-08-06 22:58:38 -0400

    refactored code into a function and added function call with options interface and newMac as arguments

--- mac.changer.py
+++ mac.changer.py
@@ -3,6 +3,14 @@
 import subprocess
 import optparse
 
+def changeMac(interface, newMac):
+    print("[+} Changing MAC addres for " + interface + " to " + newMac)
+
+    subprocess.call("ifconfig " + interface + " down", shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
+    subprocess.call("ifconfig " + interface + " up", shell=True)
+
+
 parser = optparse.OptionParser()
  
 parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
@@ -10,14 +18,4 @@ parser.add_option("-m", "--mac", dest="newMac", help="new MAC address")
 
 (options, arguments) = parser.parse_args()
 
-
-interface = options.interface
-newMac = options.newMac
-
-
-print("[+} Changing MAC addres for " + interface + " to " + newMac)
-
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-
+changeMac(options.interface, options.newMac)
