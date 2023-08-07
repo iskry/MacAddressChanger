2617c860ffc1eee92a8c7dee795468dfd6634f7a
iskry <refocuslucifer@protonmail.com>
2023-08-06 23:04:35 -0400

    created a get arguments function to clean up code

--- mac.changer.py
+++ mac.changer.py
@@ -3,19 +3,18 @@
 import subprocess
 import optparse
 
+def getArguments(): 
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="newMac", help="new MAC address")
+    return parser.parse_args()
+
 def changeMac(interface, newMac):
     print("[+} Changing MAC addres for " + interface + " to " + newMac)
-
     subprocess.call("ifconfig " + interface + " down", shell=True)
     subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
     subprocess.call("ifconfig " + interface + " up", shell=True)
 
-
-parser = optparse.OptionParser()
- 
-parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="newMac", help="new MAC address")
-
-(options, arguments) = parser.parse_args()
-
+(options, arguments) = getArguments()
 changeMac(options.interface, options.newMac)
+
