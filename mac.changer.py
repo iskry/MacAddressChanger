78e501a7b7a5d2e97ac401f88a631e4c29d77e5b
iskry <refocuslucifer@protonmail.com>
2023-08-06 23:14:59 -0400

    added conditional logic for error handling

--- mac.changer.py
+++ mac.changer.py
@@ -7,7 +7,12 @@ def getArguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="newMac", help="new MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.newMac:
+        parser.error("[-] Please specify a new MAC, use --help for more info.")
+    return options
 
 def changeMac(interface, newMac):
     print("[+} Changing MAC addres for " + interface + " to " + newMac)
@@ -15,6 +20,6 @@ def changeMac(interface, newMac):
     subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
     subprocess.call("ifconfig " + interface + " up", shell=True)
 
-(options, arguments) = getArguments()
+options = getArguments()
 changeMac(options.interface, options.newMac)
 
